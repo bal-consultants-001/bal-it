863d7bc581f59d217d1749a66730bb101d9969cb
Ben Lane <balcomputers19@gmail.com>
2026-01-08 12:07:07 +0000

    update layout 4

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,14 +21,7 @@ export default function Navbar() {
 
   return (
     <nav className="fixed top-0 left-0 w-full bg-white shadow z-50">
-      <div className="max-w-6xl mx-auto flex justify-between items-center p-4">
-        <Image
-              src="/images/bal-it.png"
-              alt="BAL-IT"
-              width="160"
-			  height="80"
-              className="object-cover"
-            />
+
 			{/*<span className="font-bold text-xl text-black">BAL-IT</span>*/}
 
         <div className="flex items-center space-x-6 text-black">
