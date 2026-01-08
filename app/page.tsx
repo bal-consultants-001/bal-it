84089b5a3bae216d82f867554e658d2b17f1084f
Ben Lane <balcomputers19@gmail.com>
2026-01-08 12:13:01 +0000

    update layout 5

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,8 @@ export default function Home() {
     <>
       <Navbar />
 
+		<div className="pt-[90px]" />
+
 		<div
 		  id="logo"
 		  className="w-full bg-white py-6 flex justify-center items-center scroll-mt-24"
