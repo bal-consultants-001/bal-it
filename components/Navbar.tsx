5b8ec8c7d5fb25e19633a20d087eba9678bce1f0
Ben Lane <balcomputers19@gmail.com>
2026-01-08 11:03:21 +0000

    update nav scroll

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -39,7 +39,7 @@ export default function Navbar() {
               to={item.to}
               spy={true}
               smooth={true}
-              offset={-120}
+              offset={-90}
               duration={500}
               activeClass="text-blue-600 font-semibold"
               className="cursor-pointer hover:text-blue-500"
