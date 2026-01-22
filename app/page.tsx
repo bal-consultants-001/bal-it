15d961d7fc291c557469fdc3ef1811392addc757
Ben Lane <balcomputers19@gmail.com>
2026-01-22 11:42:09 +0000

    Update home page

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
 			  src="/images/video-fallback.png"
 			  alt=""
 			  fill
-			  className="object-cover scale-175"
+			  className="object-cover scale-275"
 			  priority
 			/>
 		  </div>
