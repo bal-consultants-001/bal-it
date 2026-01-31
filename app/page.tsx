d825a5c22496261e8b92205ae6cb1b63154c42b4
Ben Lane <balcomputers19@gmail.com>
2026-01-31 10:56:03 +0000

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -92,7 +92,7 @@ export default function Home() {
 
 		<div
 		  id="image"
-		  className="relative w-full h-[400px]"
+		  className="relative w-full aspect-[16/9] md:aspect-[21/9]"
 		>
 		  <Image
 			src="/images/backdrop.jpg"
